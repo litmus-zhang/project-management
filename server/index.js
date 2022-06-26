b773f03f835c1240f27958e67ee590a64d005340
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-06-26 07:57:50 +0100

    connected mongoose model

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
-require('dotenv').config();
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+// console.log(process.env.NODE_ENV);
 const port = process.env.PORT || 5000
+const colors = require('colors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 
 const app = express();
 
+
+//COnnec to mongoDB
+connectDB();
+
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV !== 'development' ? true : false
+    graphiql: process.env.NODE_ENV === 'development',
 }))
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,8 @@
-const { projects, clients } = require('../sampleData');
+//Mongoose Models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
-const {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema} = require('graphql');
+const {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLList} = require('graphql');
 
 
 //Client Type
@@ -13,11 +15,52 @@ const ClientType = new GraphQLObjectType({
         phone: { type: GraphQLString }
     })
 })
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args)
+            { 
+                return clients.findById(client => client.id === parent.clientId)
+            }
+        }
+    })
+})
 
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args)
+            { 
+             return  Project.find()
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLID },
+            },
+            resolve(parent, args)
+            {
+                return Project.findById(args.id)
+             }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args)
+            { 
+                return Client.find()   
+            }
+        },
         client: {
             type: ClientType,
             args: {
@@ -25,11 +68,19 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args)
             {
-                return clients.find(client => client.id === args.id)
+                return Client.findById(args.id)
              }
-        }
+        },
     }
 })
+
+const RootMutation = new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+        
+    }
+})
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
