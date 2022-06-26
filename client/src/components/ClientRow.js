50c8c3da2cead0dda07032f4bdeeb4ac46f2062d
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-06-27 00:11:29 +0100

    All row works fine

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -2,12 +2,13 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutation';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client })
 {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{query : GET_CLIENTS}],
+    // refetchQueries: [{query : GET_CLIENTS}, {query : GET_PROJECTS}],
     update(cache, { data: { deleteClient } })
     {
       const { clients } = cache.readQuery({ query: GET_CLIENTS });
@@ -15,7 +16,7 @@ export default function ClientRow({ client })
         query: GET_CLIENTS,
         data: { clients: clients.filter(c => c.id !== deleteClient.id) }
       }
-      )
+      )        
     }
   });
   return (
