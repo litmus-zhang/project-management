644525e91ed33c54237b89bd258b10200015abd3
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-07-14 08:34:42 +0100

    everything sounds good

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ const cache = new InMemoryCache({
 
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: "/graphql",
   cache,
 });
 
--- server/index.js
+++ server/index.js
@@ -23,6 +23,11 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development',
 }))
+app.use(express.static('public'));
+app.get("*" , (req, res) =>
+{ 
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 
 app.listen(port,  console.log(`Listening on port ${port}`));
